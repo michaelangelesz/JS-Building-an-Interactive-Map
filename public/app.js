fc36760fc61d0be691de74b8d86767f4c03211d3
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-03 10:14:25 -0400

    created map

--- public/app.js
+++ public/app.js
@@ -2,7 +2,12 @@
 
 
 
-// Create map
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
+
 
 
 
